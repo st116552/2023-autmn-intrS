7b8fdab9fdb8f7424eeacbc4da4a9947e74fb637
st116552 <144111896+st116552@users.noreply.github.com>
2023-12-24 12:38:33 +0300

    23.12.24 12:37

--- 2023.10.26/main.py
+++ 2023.10.26/main.py
@@ -1,34 +1,34 @@
 import numpy
 import pandas
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 from sklearn import tree
 from sklearn.model_selection import train_test_split
 
 
 def mse(y, y_predicted):
-    return numpy.square(y - y_predicted) / len(y)
+    return numpy.mean(y - y_predicted) / len(y)
 
 
 def mae(y, y_predicted):
-    return numpy.absolute(y - y_predicted) / len(y)
+    return numpy.mean(y - y_predicted) / len(y)
 
 
 def count_metrics(y, y_predicted):
-    return [mse(y, y_predicted), mae(y, y_predicted)]
+    return {'MSE': mse(y, y_predicted),
+            'MAE': mae(y, y_predicted)}
 
 
 def draw_graph(x_arrays, y_arrays, names):
-    line_colors = ['b', 'g', 'r', 'c', 'm', 'y']
-    i = 0
-    while i < len(x_arrays) and i < len(y_arrays) and i < len(names) and i < len(line_colors):
-        line_width = 1 + i
-        matplotlib.pyplot.plot(x_arrays[i], y_arrays[i], label=names[i], color=line_colors[i], linewidth=line_width)
-        i += 1
-    matplotlib.pyplot.xlabel("y, m")
-    matplotlib.pyplot.ylabel('v, m/s')
-    matplotlib.pyplot.grid()
-    matplotlib.pyplot.legend()
-    matplotlib.pyplot.show()
+    line_colors = ['b', 'y', 'c', 'g', 'r']
+    # i = 0
+
+    for i in range(len(x_arrays)):
+        plt.plot(x_arrays[i], y_arrays[i], 'o', label=names[i], color=line_colors[i], linewidth=0.5)
+    plt.xlabel("y, m")
+    plt.ylabel('v, m/s')
+    plt.grid()
+    plt.legend()
+    plt.show()
 
 
 def calc_decision_tree_regression(exp_data_file_name, data_file_name):
@@ -36,9 +36,9 @@ def calc_decision_tree_regression(exp_data_file_name, data_file_name):
     exp_data_f = pandas.read_csv(exp_data_file_name)
     data_f.head()
     exp_data_f.head()
-    x = data_f['y']
+    x = data_f['y'].to_numpy().reshape((-1, 1))
     y = data_f['V']
-    x_exp = exp_data_f['y']
+    x_exp = exp_data_f['y'].to_numpy().reshape((-1, 1))
     y_exp = exp_data_f['V']
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
@@ -54,13 +54,13 @@ def calc_decision_tree_regression(exp_data_file_name, data_file_name):
     metrics_exp = count_metrics(y_exp, y_exp_predicted)
 
     print('mse ans mae')
-    print('train', metrics_train)
-    print('test', metrics_test)
-    print('exp', metrics_exp)
-    draw_graph([x_train, x_test, x_exp], [y_train, y_test_predicted, y_exp], ['Dataset', 'Predicts', 'Experiment'])
+    print(f'Train: MSE = {metrics_train["MSE"]}, MAE = {metrics_train["MAE"]}')
+    print(f'Test: MSE = {metrics_test["MSE"]}, MAE = {metrics_test["MAE"]}')
+    print(f'Experiment: MSE = {metrics_exp["MSE"]}, MAE = {metrics_exp["MAE"]}')
+
+    draw_graph([x_train, x_test, x_exp], [y_train, y_test_predicted, y_exp],
+               ['Dataset', 'Predicts', 'Experiment'])
 
 
-
-#calc_decision_tree_regression('couetteRe1200.csv', 'dataRe1200.csv')
+calc_decision_tree_regression('dataRe1200.csv', 'couetteRe1200.csv')
 calc_decision_tree_regression('poiseuilleDataRe1200.csv', 'resultsRe1200.csv')
-
